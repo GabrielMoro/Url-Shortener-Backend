700cb289a6f4de8ccee72cccab595aa1a4ea3116
GabrielMoro <gabrielhmoro@gmail.com>
2025-04-17 20:37:24 -0300

    feat: adicionando swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const logger = new Logger()
+
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle('URL Shortener API')
+  .setDescription('API para encurtamento de URLs')
+  .setVersion('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document); // http://localhost:3000/api/docs
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  logger.log(`Aplicação rodando em http://localhost:${port}`);
+  logger.log(`Swagger em http://localhost:${port}/api/docs`);
 }
 bootstrap();
