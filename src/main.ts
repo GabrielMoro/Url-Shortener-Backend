d53b0e0b71e5a12f68fa86b87e2224d8cf7f1079
GabrielMoro <gabrielhmoro@gmail.com>
2025-04-17 21:38:09 -0300

    feat: configuração de variáveis de ambiente

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+
   const app = await NestFactory.create(AppModule);
 
-  const logger = new Logger()
+  const logger = new Logger();
 
   const swaggerConfig = new DocumentBuilder()
-  .setTitle('URL Shortener API')
-  .setDescription('API para encurtamento de URLs')
-  .setVersion('1.0')
-  .build();
+    .setTitle('URL Shortener API')
+    .setDescription('API para encurtamento de URLs')
+    .setVersion('1.0')
+    .build();
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api/docs', app, document); // http://localhost:3000/api/docs
