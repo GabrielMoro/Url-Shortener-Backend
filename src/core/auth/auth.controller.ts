671e566e4316ef67d33a91c9d6ae7b74025c39b8
GabrielMoro <gabrielhmoro@gmail.com>
2025-04-22 16:38:03 -0300

    feat: registro de usuário

--- src/core/auth/auth.controller.ts
+++ src/core/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { AuthService } from './services/auth/auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  public constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  public register(@Body() input: CreateUserDto) {
+    return this.authService.register(input);
+  }
+}
--- src/core/auth/services/auth/auth.service.ts
+++ src/core/auth/services/auth/auth.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from '../../dtos/create-user.dto';
+import { User } from '@/core/user/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as crypto from 'crypto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  public constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async register(input: CreateUserDto): Promise<User> {
+    const emailInUse = await this.userRepository.findOne({ where: { email: input.email } });
+    if (emailInUse) {
+      throw new BadRequestException('Email já cadastrado');
+    }
+
+    const hashedPassword = crypto.createHash('sha256').update(input.password).digest('hex');
+    const user = this.userRepository.create({ email: input.email, password: hashedPassword });
+
+    return this.userRepository.save(user);
+  }
+}
