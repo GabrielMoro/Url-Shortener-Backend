8a684e72066bec9e5ca70ead6f6dbd7fa16685fc
GabrielMoro <gabrielhmoro@gmail.com>
2025-04-22 13:59:23 -0300

    feat: add typeorm + create entities

--- src/core/url/services/url/url.service.ts
+++ src/core/url/services/url/url.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Url } from '../../entities/url.entity';
 
 @Injectable()
-export class UrlService {}
+export class UrlService {
+  public constructor(
+    @InjectRepository(Url)
+    private readonly urlRepository: Repository<Url>,
+  ) {}
+}
