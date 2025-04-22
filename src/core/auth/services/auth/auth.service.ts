647389f3c35dd5e7c72f342a16a2036fe011f512
GabrielMoro <gabrielhmoro@gmail.com>
2025-04-22 19:37:54 -0300

    chore: adicionando logs

--- src/core/auth/services/auth/auth.service.ts
+++ src/core/auth/services/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { CreateUserDto } from '../../dtos/create-user.dto';
 import { User } from '@/core/user/entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -10,16 +10,23 @@ export class AuthService {
   public constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly logger: Logger,
   ) {}
 
   async register(input: CreateUserDto): Promise<User> {
+    this.logger.log('Iniciando cadastro de usuário');
+
     const emailInUse = await this.userRepository.findOne({ where: { email: input.email } });
     if (emailInUse) {
+      this.logger.error('Email já cadastrado', { email: input.email });
+
       throw new BadRequestException('Email já cadastrado');
     }
 
     const hashedPassword = crypto.createHash('sha256').update(input.password).digest('hex');
     const user = this.userRepository.create({ email: input.email, password: hashedPassword });
+
+    this.logger.log('Novo usuário cadastrado');
 
     return this.userRepository.save(user);
   }
