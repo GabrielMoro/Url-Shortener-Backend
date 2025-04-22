d4118f316ef1f11a9d9bb8ef2dda795bd01fd627
GabrielMoro <gabrielhmoro@gmail.com>
2025-04-22 13:56:44 -0300

    chore: remove prisma

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,23 @@
+import { Url } from '@/core/url/entities/url.entity';
+import { User } from '@/core/user/entities/user.entity';
 import { Global, Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Global()
 @Module({
-  imports: [PrismaModule]
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        entities: [User, Url],
+        synchronize: true,
+        logging: true,
+      }),
+    }),
+  ],
 })
 export class DatabaseModule {}
