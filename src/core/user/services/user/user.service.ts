d98c575c34ce8e60abfce60415bba8ea048ec698
GabrielMoro <gabrielhmoro@gmail.com>
2025-04-23 17:03:44 -0300

    feat: rota para retornar informação de uma url específica

--- src/core/user/services/user/user.service.ts
+++ src/core/user/services/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../../entities/user.entity';
@@ -36,5 +36,39 @@ export class UserService {
       clicks: url.clicks,
       createdAt: url.createdAt,
     }));
+  }
+
+  async getUrlByShortCode(userId: string, shortCode: string): Promise<ListUrlDto> {
+    this.logger.log('Iniciando busca de URL pelo shortCode');
+
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+      relations: ['urls'],
+    });
+
+    if (!user) {
+      this.logger.error('Usuário não encontrado');
+
+      throw new NotFoundException('URL não encontrada');
+    }
+
+    const url = user.urls.find((url) => url.shortCode === shortCode);
+
+    if (!url) {
+      this.logger.error('URL não encontrada');
+
+      throw new NotFoundException('URL não encontrada');
+    }
+
+    const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
+
+    return {
+      id: url.id,
+      shortCode: url.shortCode,
+      shortUrl: `${baseUrl}/${url.shortCode}`,
+      targetUrl: url.targetUrl,
+      clicks: url.clicks,
+      createdAt: url.createdAt,
+    };
   }
 }
--- src/core/user/user.controller.ts
+++ src/core/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { AuthGuard } from '@/infra/guard/authorization.guard';
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UserService } from './services/user/user.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GetUser } from '@/common/decorators/get-user.decorator';
@@ -22,7 +22,28 @@ export class UserController {
     type: [ListUrlDto],
   })
   @Get('urls')
-  async urls(@GetUser() user: User): Promise<ListUrlDto[]> {
+  async listUrls(@GetUser() user: User): Promise<ListUrlDto[]> {
     return this.userService.listUrls(user.id);
+  }
+
+  @ApiOperation({
+    summary: 'Obter uma URL encurtada',
+    description: 'Retorna uma URL encurtada específica com base no shortCode.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'URL encontrada e retornada com sucesso.',
+    type: ListUrlDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'URL não encontrada.',
+  })
+  @Get('url/:shortCode')
+  async getOneUrl(
+    @Param('shortCode') shortCode: string,
+    @GetUser() user: User,
+  ): Promise<ListUrlDto> {
+    return this.userService.getUrlByShortCode(user.id, shortCode);
   }
 }
