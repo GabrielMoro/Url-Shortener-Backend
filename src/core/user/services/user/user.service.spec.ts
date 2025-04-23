555073bc359c714c03c7123cbc29cc8f59b08e2b
GabrielMoro <gabrielhmoro@gmail.com>
2025-04-23 16:29:24 -0300

    test: testes unitários para listUrls

--- src/core/user/services/user/user.service.spec.ts
+++ src/core/user/services/user/user.service.spec.ts
@@ -1,18 +1,89 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../../entities/user.entity';
 
 describe('UserService', () => {
   let service: UserService;
+  let userRepository: jest.Mocked<Repository<User>>;
 
   beforeEach(async () => {
+    userRepository = {
+      findOne: jest.fn(),
+    } as unknown as jest.Mocked<Repository<User>>;
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: userRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    userRepository = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('listUrls', () => {
+    it('Deve retornar uma lista de URLs para o usuário', async () => {
+      const mockUser = {
+        id: 'user-id',
+        urls: [
+          {
+            id: 'url-id',
+            shortCode: 'abc123',
+            targetUrl: 'https://example.com',
+            clicks: 10,
+            createdAt: new Date('2023-01-01T00:00:00.000Z'),
+          },
+        ],
+      } as unknown as User;
+
+      userRepository.findOne.mockResolvedValue(mockUser);
+
+      const result = await service.listUrls('user-id');
+
+      expect(userRepository.findOne).toHaveBeenCalledWith({
+        where: { id: 'user-id' },
+        relations: ['urls'],
+      });
+
+      expect(result).toEqual([
+        {
+          id: 'url-id',
+          shortCode: 'abc123',
+          shortUrl: 'http://localhost:3000/abc123',
+          targetUrl: 'https://example.com',
+          clicks: 10,
+          createdAt: new Date('2023-01-01T00:00:00.000Z'),
+        },
+      ]);
+    });
+
+    it('Deve retornar um array vazio se o usuário não tiver URLs', async () => {
+      const mockUser = { id: 'user-id', urls: [] } as unknown as User;
+
+      userRepository.findOne.mockResolvedValue(mockUser);
+
+      const result = await service.listUrls('user-id');
+
+      expect(result).toEqual([]);
+    });
+
+    it('deve retornar um array vazio se o usuário não for encontrado', async () => {
+      userRepository.findOne.mockResolvedValue(null);
+
+      const result = await service.listUrls('inexistente');
+
+      expect(result).toEqual([]);
+    });
   });
 });
