ec4bceee184bfceaa5849fe6d866f2af89d24952
GabrielMoro <gabrielhmoro@gmail.com>
2025-04-23 16:13:17 -0300

    feat: listagem de urls de usuário

--- src/core/user/services/user/user.service.ts
+++ src/core/user/services/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../../entities/user.entity';
+import { Url } from '@/core/url/entities/url.entity';
 
 @Injectable()
 export class UserService {
@@ -9,4 +10,13 @@ export class UserService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
   ) {}
+
+  async listUrls(userId: string): Promise<Url[]> {
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+      relations: ['urls'],
+    });
+
+    return user?.urls || [];
+  }
 }
--- src/core/user/user.controller.ts
+++ src/core/user/user.controller.ts
@@ -1,6 +1,18 @@
 import { AuthGuard } from '@/infra/guard/authorization.guard';
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { UserService } from './services/user/user.service';
+import { ApiTags } from '@nestjs/swagger';
+import { GetUser } from '@/common/decorators/get-user.decorator';
+import { User } from './entities/user.entity';
 
+@ApiTags('user')
 @UseGuards(AuthGuard)
 @Controller('user')
-export class UserController {}
+export class UserController {
+  public constructor(private readonly userService: UserService) {}
+
+  @Get('urls')
+  async urls(@GetUser() user: User) {
+    return this.userService.listUrls(user.id);
+  }
+}
