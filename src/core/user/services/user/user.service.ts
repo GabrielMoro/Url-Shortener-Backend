9c802a11a5758f42c4f97a32fe3d9941db1754b1
GabrielMoro <gabrielhmoro@gmail.com>
2025-06-25 23:21:30 -0300

    refactor: adicionados tratamentos de erro, e otimização de queries e logs

--- src/core/user/services/user/user.service.ts
+++ src/core/user/services/user/user.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, Injectable, Logger, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../../entities/user.entity';
@@ -17,24 +22,34 @@ export class UserService {
     private readonly logger: Logger,
   ) {}
 
-  private readonly DEFAULT_BASE_URL = 'http://localhost:3000';
+  private readonly DEFAULT_BASE_URL = 'http://localhost';
 
-  async listUrls(userId: string): Promise<ListUrlDto[]> {
+  public async listUrls(userId: string): Promise<ListUrlDto[]> {
     this.logger.log('Iniciando listagem de URLs');
 
     const user = await this.userRepository
       .createQueryBuilder('user')
       .leftJoinAndSelect('user.urls', 'url', 'url.deletedAt IS NULL')
       .where('user.id = :userId', { userId })
-      .getOne();
+      .getOne()
+      .catch((error) => {
+        this.logger.error('Erro ao buscar URLs do usuário', error);
+        throw new InternalServerErrorException('Erro ao buscar URLs do usuário');
+      });
 
-    if (!user?.urls) {
+    if (!user) {
+      this.logger.warn('Usuário não encontrado', userId);
+
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    if (!user.urls.length) {
       this.logger.log('Usuário não possui URLs');
 
       return [];
     }
 
-    const baseUrl = process.env.BASE_URL || this.DEFAULT_BASE_URL;
+    const baseUrl = process.env.BASE_URL ?? `${this.DEFAULT_BASE_URL}:${process.env.PORT ?? 3000}`;
 
     return user.urls.map((url) => ({
       id: url.id,
@@ -43,33 +58,32 @@ export class UserService {
       targetUrl: url.targetUrl,
       clicks: url.clicks,
       createdAt: url.createdAt,
+      updatedAt: url.updatedAt,
     }));
   }
 
-  async getUrlByShortCode(userId: string, shortCode: string): Promise<ListUrlDto> {
+  public async getUrlByShortCode(userId: string, shortCode: string): Promise<ListUrlDto> {
     this.logger.log('Iniciando busca de URL pelo shortCode');
 
-    const user = await this.userRepository
-      .createQueryBuilder('user')
-      .leftJoinAndSelect('user.urls', 'url', 'url.deletedAt IS NULL')
-      .where('user.id = :userId', { userId })
-      .getOne();
-
-    if (!user) {
-      this.logger.error('Usuário não encontrado');
-
-      throw new NotFoundException('URL não encontrada');
-    }
-
-    const url = user.urls.find((url) => url.shortCode === shortCode);
+    const url = await this.urlRepository
+      .createQueryBuilder('url')
+      .where('url.shortCode = :shortCode', { shortCode })
+      .andWhere('url.deletedAt IS NULL')
+      .leftJoinAndSelect('url.user', 'user')
+      .andWhere('user.id = :userId', { userId })
+      .getOne()
+      .catch((error) => {
+        this.logger.error('Erro ao buscar URLs do usuário', error);
+        throw new InternalServerErrorException('Erro ao buscar URLs do usuário');
+      });
 
     if (!url) {
-      this.logger.error('URL não encontrada');
+      this.logger.warn('URL não encontrada ou não pertence ao usuário', shortCode);
 
-      throw new NotFoundException('URL não encontrada');
+      throw new NotFoundException('URL não encontrada ou não pertence ao usuário');
     }
 
-    const baseUrl = process.env.BASE_URL || this.DEFAULT_BASE_URL;
+    const baseUrl = process.env.BASE_URL ?? `${this.DEFAULT_BASE_URL}:${process.env.PORT ?? 3000}`;
 
     return {
       id: url.id,
@@ -78,81 +92,80 @@ export class UserService {
       targetUrl: url.targetUrl,
       clicks: url.clicks,
       createdAt: url.createdAt,
+      updatedAt: url.updatedAt,
     };
   }
 
-  async updateOneUrl(userId: string, body: UpdateUrlDto): Promise<ListUrlDto> {
+  public async updateOneUrl(userId: string, body: UpdateUrlDto): Promise<ListUrlDto> {
     this.logger.log('Iniciando atualização de URL');
 
     const { newUrl, shortCode } = body;
 
-    const user = await this.userRepository
-      .createQueryBuilder('user')
-      .leftJoinAndSelect('user.urls', 'url', 'url.deletedAt IS NULL')
-      .where('user.id = :userId', { userId })
-      .getOne();
-
-    if (!user) {
-      this.logger.error('Usuário não encontrado');
-
-      throw new NotFoundException('Usuário não encontrado');
-    }
-
-    const url = user.urls.find((url) => url.shortCode === shortCode);
+    const url = await this.urlRepository
+      .createQueryBuilder('url')
+      .where('url.shortCode = :shortCode', { shortCode })
+      .andWhere('url.deletedAt IS NULL')
+      .leftJoinAndSelect('url.user', 'user')
+      .andWhere('user.id = :userId', { userId })
+      .getOne()
+      .catch((error) => {
+        this.logger.error('Erro ao buscar URLs do usuário', error);
+        throw new InternalServerErrorException('Erro ao buscar URLs do usuário');
+      });
 
     if (!url) {
-      this.logger.error('URL não encontrada');
+      this.logger.warn('URL não encontrada ou não pertence ao usuário', shortCode);
 
-      throw new NotFoundException('URL não encontrada');
+      throw new NotFoundException('URL não encontrada ou não pertence ao usuário');
     }
 
     url.targetUrl = newUrl;
 
     try {
-      this.logger.log('Atualizando URL');
+      this.logger.log(`Atualizando URL com shortCode: ${shortCode} para: ${newUrl}`);
 
-      await this.urlRepository.save(url);
+      const updatedUrl = await this.urlRepository.save(url);
+
+      const baseUrl =
+        process.env.BASE_URL ?? `${this.DEFAULT_BASE_URL}:${process.env.PORT ?? 3000}`;
+
+      return {
+        id: updatedUrl.id,
+        shortCode: updatedUrl.shortCode,
+        shortUrl: `${baseUrl}/${updatedUrl.shortCode}`,
+        targetUrl: updatedUrl.targetUrl,
+        clicks: updatedUrl.clicks,
+        createdAt: updatedUrl.createdAt,
+        updatedAt: updatedUrl.updatedAt,
+      };
     } catch (error) {
       this.logger.error('Erro ao atualizar a URL', error);
 
-      throw new BadRequestException('Erro ao atualizar a URL');
+      throw new InternalServerErrorException('Erro ao atualizar a URL');
     }
-
-    const baseUrl = process.env.BASE_URL || this.DEFAULT_BASE_URL;
-
-    return {
-      id: url.id,
-      shortCode: url.shortCode,
-      shortUrl: `${baseUrl}/${url.shortCode}`,
-      targetUrl: url.targetUrl,
-      clicks: url.clicks,
-      createdAt: url.createdAt,
-    };
   }
 
-  async deleteOneUrl(userId: string, body: DeleteUrlDto): Promise<ListUrlDto> {
+  public async deleteOneUrl(userId: string, body: DeleteUrlDto): Promise<ListUrlDto> {
     this.logger.log('Iniciando exclusão de URL');
 
     const { shortCode } = body;
 
-    const user = await this.userRepository
-      .createQueryBuilder('user')
-      .leftJoinAndSelect('user.urls', 'url', 'url.deletedAt IS NULL')
-      .where('user.id = :userId', { userId })
-      .getOne();
-
-    if (!user) {
-      this.logger.error('Usuário não encontrado');
-
-      throw new NotFoundException('Usuário não encontrado');
-    }
-
-    const url = user.urls.find((url) => url.shortCode === shortCode);
+    const url = await this.urlRepository
+      .createQueryBuilder('url')
+      .where('url.shortCode = :shortCode', { shortCode })
+      .andWhere('url.deletedAt IS NULL')
+      .leftJoinAndSelect('url.user', 'user')
+      .andWhere('user.id = :userId', { userId })
+      .getOne()
+      .catch((error) => {
+        this.logger.error('Erro ao buscar URLs do usuário', error);
+        throw new InternalServerErrorException('Erro ao buscar URLs do usuário');
+      });
 
     if (!url) {
-      this.logger.error('URL não encontrada');
+      this.logger.warn('URL não encontrada ou não pertence ao usuário', shortCode);
 
-      throw new NotFoundException('URL não encontrada');
+      throw new NotFoundException('URL não encontrada ou não pertence ao usuário');
     }
 
     url.deletedAt = new Date();
@@ -160,23 +173,29 @@ export class UserService {
     try {
       this.logger.log(`Marcando URL como deletada: ${shortCode}`);
 
-      await this.urlRepository.save(url);
+      const deletedUrl = await this.urlRepository.save(url);
+
+      const baseUrl =
+        process.env.BASE_URL ?? `${this.DEFAULT_BASE_URL}:${process.env.PORT ?? 3000}`;
+
+      return {
+        id: deletedUrl.id,
+        shortCode: deletedUrl.shortCode,
+        shortUrl: `${baseUrl}/${deletedUrl.shortCode}`,
+        targetUrl: deletedUrl.targetUrl,
+        clicks: deletedUrl.clicks,
+        createdAt: deletedUrl.createdAt,
+        updatedAt: deletedUrl.updatedAt,
+        deletedAt: deletedUrl.deletedAt,
+      };
     } catch (error) {
-      this.logger.error('Erro ao realizar exclusão lógica da URL', error);
+      this.logger.error(
+        'Erro ao realizar exclusão lógica da URL',
+        (error as Error).message,
+        (error as Error).stack,
+      );
 
-      throw new BadRequestException('Erro ao deletar a URL');
+      throw new InternalServerErrorException('Erro ao deletar a URL');
     }
-
-    const baseUrl = process.env.BASE_URL || this.DEFAULT_BASE_URL;
-
-    return {
-      id: url.id,
-      shortCode: url.shortCode,
-      shortUrl: `${baseUrl}/${url.shortCode}`,
-      targetUrl: url.targetUrl,
-      clicks: url.clicks,
-      createdAt: url.createdAt,
-      deletedAt: url.deletedAt,
-    };
   }
 }
