7a274ecd3bf54e080ed40f2b3d2fb8e312465a0f
GabrielMoro <gabrielhmoro@gmail.com>
2025-06-25 23:19:14 -0300

    feat: criação de base entity contendo id e timestamps

---
+++ src/common/entities/base.entity.ts
@@ -0,0 +1,22 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column({ nullable: true })
+  deletedAt?: Date;
+}
